905caa1c2e3552e5feaaa82c1cbed5b7bb08228c
唐子抗 <fromwherepromax@users.noreply.github.com>
2026-05-07 16:20:45 +0800

    为背包里面物品添加详细信息，3个按钮：use drop close

--- My-RPG/Assets/Scripts/Inventory&Shop/ItemSos/InventorySlot.cs
+++ My-RPG/Assets/Scripts/Inventory&Shop/ItemSos/InventorySlot.cs
@@ -1,77 +1,53 @@
-
-using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class InventorySlot : MonoBehaviour,IPointerClickHandler
-{   
+public class InventorySlot : MonoBehaviour, IPointerClickHandler
+{
     public ItemSo itemSo;
     public int quantity;
     public Image itemImage;
     public TMP_Text quantityText;
-    private InventoryManager inventoryManager;
-    private ShopManager activeShop;
+
+    private InventoryDetailsUI detailsUI;
 
     private void Start()
-    {   
-        inventoryManager = GetComponentInParent<InventoryManager>();
-        UpdateUI();
-    }
-    // private void OnEnable()
-    // {
-    //     ShopKeeper.OnShopStateChanged += HandleShopStateChanged;
-    // }
-    // private void OnDisable()
-    // {
-    //     ShopKeeper.OnShopStateChanged -= HandleShopStateChanged;
-    // }
-    private void HandleShopStateChanged(ShopManager shopManager, bool isOpen)
     {
-        if (isOpen)
-        {
-            activeShop = shopManager;
-        }
-        else if (activeShop == shopManager)
-        {
-            activeShop = null;
-        }
+        detailsUI = GetComponentInParent<InventoryDetailsUI>();
+        UpdateUI();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (quantity > 0)
-        {   
-            if (eventData.button == PointerEventData.InputButton.Left)
-            {   
-                if(activeShop != null)
-                {
-                    activeShop.SellItem(itemSo);
-                    quantity--;
-                    UpdateUI();
-                }
-                else{
-                    inventoryManager.UseItem(this); 
-                }
-            }
-            else if (eventData.button == PointerEventData.InputButton.Right)
-            {
-                inventoryManager.DropItem(this);
-            }
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            SelectSlot();
         }
     }
 
-    public void UpdateUI()  //更新背包格子UI显示
-    {   
-        if(quantity <= 0)
+    private void SelectSlot()
+    {
+        if (quantity <= 0 || itemSo == null || detailsUI == null)
+        {
+            return;
+        }
+
+        detailsUI.SelectSlot(this);
+    }
+
+    public void UpdateUI()
+    {
+        if (quantity <= 0)
         {
             itemSo = null;
         }
-        if(itemSo != null)
+
+        if (itemSo != null)
         {
-           itemImage.sprite = itemSo.itemIcon;
-           itemImage.gameObject.SetActive(true);
-           quantityText.text =quantity.ToString();
+            itemImage.sprite = itemSo.itemIcon;
+            itemImage.gameObject.SetActive(true);
+            quantityText.text = quantity.ToString();
         }
         else
         {
--- My-RPG/Assets/Scripts/Inventory&Shop/ItemSos/UseItem.cs
+++ My-RPG/Assets/Scripts/Inventory&Shop/ItemSos/UseItem.cs
@@ -1,21 +1,26 @@
-// 脚本说明：UseItem 相关逻辑。
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UseItem : MonoBehaviour
 {
     public void ApplyItemEffect(ItemSo itemSo)
     {
-        if(itemSo.currentHealth > 0)
+        if (itemSo == null)
+        {
+            return;
+        }
+
+        if (itemSo.currentHealth != 0)
         {
             StatsManager.Instance.UpdateHealth(itemSo.currentHealth);
         }
-        if(itemSo.speed > 0)
+
+        if (itemSo.speed != 0)
         {
             StatsManager.Instance.UpdateSpeed(itemSo.speed);
         }
-        if(itemSo.duration > 0)
+
+        if (itemSo.duration > 0)
         {
             StartCoroutine(EffectTimer(itemSo, itemSo.duration));
         }
@@ -24,11 +29,13 @@ public class UseItem : MonoBehaviour
     private IEnumerator EffectTimer(ItemSo itemSo, float duration)
     {
         yield return new WaitForSeconds(duration);
-         if(itemSo.currentHealth > 0)
+
+        if (itemSo.currentHealth != 0)
         {
             StatsManager.Instance.UpdateHealth(-itemSo.currentHealth);
         }
-        if(itemSo.speed > 0)
+
+        if (itemSo.speed != 0)
         {
             StatsManager.Instance.UpdateSpeed(-itemSo.speed);
         }
