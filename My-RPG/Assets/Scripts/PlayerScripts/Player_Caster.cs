338246b592074f29efc2e70d10c9b912497e87b5
唐子抗 <15216902+ZiKang-tang@user.noreply.gitee.com>
2026-06-07 20:46:48 +0800

    给Caster的Explosion加上90%生命值的条件

--- My-RPG/Assets/Scripts/PlayerScripts/Player_Caster.cs
+++ My-RPG/Assets/Scripts/PlayerScripts/Player_Caster.cs
@@ -8,11 +8,19 @@ public class Player_Caster : MonoBehaviour
     public GameObject explosionPrefab;
     private float castTimer;
 
+    [Header("生命值消耗")]
+    [Tooltip("释放爆炸扣除的生命值比例（0.9 = 90%）")]
+    public float hpCostRatio = 0.9f;
+
     void Update()
     {
         castTimer -= Time.deltaTime;
 
-        if (Input.GetMouseButtonDown(0) && castTimer <= 0)
+        // 生命值必须大于90%才能释放
+        bool hasEnoughHp = StatsManager.Instance != null
+            && StatsManager.Instance.CurrentHealth > StatsManager.Instance.MaxHealth * hpCostRatio;
+
+        if (Input.GetMouseButtonDown(0) && castTimer <= 0 && hasEnoughHp)
         {
             anim.SetBool("isCasting", true);
         }
@@ -55,6 +63,14 @@ public class Player_Caster : MonoBehaviour
 
         castTimer = cooldown;
         anim.SetBool("isCasting", false);
+
+        // 扣除90%生命值
+        int hpCost = Mathf.FloorToInt(StatsManager.Instance.CurrentHealth * hpCostRatio);
+        PlayerHealth playerHealth = GetComponentInParent<PlayerHealth>();
+        if (playerHealth != null && hpCost > 0)
+        {
+            playerHealth.ChangHealth(-hpCost);
+        }
     }
 
     public void FinishCast()
