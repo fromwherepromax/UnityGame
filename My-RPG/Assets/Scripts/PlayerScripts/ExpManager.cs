be8007659b3de0f1a35a4cfe0ee62c44d24a6612
唐子抗 <fromwherepromax@users.noreply.github.com>
2026-06-07 14:23:02 +0800

    增加经验管理器，升级奖励，修复敌人死亡卡死bug

---
+++ My-RPG/Assets/Scripts/PlayerScripts/ExpManager.cs
@@ -0,0 +1,64 @@
+// 脚本说明：ExpManager 相关逻辑。
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class ExpManager : MonoBehaviour
+{
+   public int level;
+    public int currentExp = 0;
+    public int expToNextLevel = 10;
+    public float expMultiplier = 1.2f;
+    public Slider expSlider;
+    public TMP_Text currentLevelText;
+    public static event Action<int> OnLevelUp;
+
+    public void Start()
+    {
+        UpdateUI();
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GainExp(2);
+        }
+    }
+    private void OnEnable()
+    {
+        Enemy_Health.OnEnemyDefeated += GainExp;
+        InventoryManager.OnExpGained += GainExp;
+    }
+    private void OnDisable()
+    {
+        Enemy_Health.OnEnemyDefeated -= GainExp;
+        InventoryManager.OnExpGained -= GainExp;
+    }
+
+    public void GainExp(int amount)
+    {
+        currentExp += amount;
+        if (currentExp >= expToNextLevel)
+        {
+            LevelUp();
+        }
+        UpdateUI();
+    }
+
+    private void LevelUp()
+    {
+        level++;
+        currentExp -= expToNextLevel;
+        expToNextLevel = Mathf.RoundToInt(expToNextLevel * expMultiplier);
+        OnLevelUp?.Invoke(1);
+    }
+    public void UpdateUI()
+    {
+        expSlider.maxValue=expToNextLevel;
+        expSlider.value=currentExp;
+        currentLevelText.text="Level: "+level.ToString();
+    }
+}
