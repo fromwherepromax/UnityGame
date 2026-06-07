9a2ae4aa15deb5dc4cfd51cc213423a7ca6e913f
唐子抗 <fromwherepromax@users.noreply.github.com>
2026-06-07 17:04:23 +0800

    把攻击改成鼠标左键，修复Archer攻击bug

--- My-RPG/Assets/Scripts/PlayerScripts/Player_Change.cs
+++ My-RPG/Assets/Scripts/PlayerScripts/Player_Change.cs
@@ -6,11 +6,19 @@ public class Player_Change : MonoBehaviour
 {
     public player_Combat combat;
     public Player_Bow bow;
+    public float switchCooldown = 0.5f;
+    private float switchTimer;
 
     void Update()
     {
-        if (Input.GetButtonDown("ChangeEquipment"))
+        if (switchTimer > 0)
         {
+            switchTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("ChangeEquipment") && switchTimer <= 0)
+        {
+            switchTimer = switchCooldown;
             if (combat.enabled)
             {
                 combat.enabled = false;
